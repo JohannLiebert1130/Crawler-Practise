6c71cec373fe6e7bb122ad7f8e4a3ceff5b007de
JohannLiebert1130 <672016243@qq.com>
2018-04-30 19:13:46 +0800

    a crawler for getting the info about univ rating

---
+++ universitiesRating/main.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class BestChinaUniv:
+    def __init__(self):
+        self.univ_list = []
+
+    def get_html(self, url):
+        r = requests.get(url)
+        r.encoding = r.apparent_encoding
+        self.html = r.text
+        return self.html
+
+    def fill_univ_list(self):
+        soup = BeautifulSoup(self.html, 'html.parser')
+        trs = soup.find_all('tr', class_='alt')
+        for tr in trs:
+            tds = tr('td')
+            self.univ_list.append([tds[0].string, tds[1].string, tds[3].string])
+
+    def print_univ_list(self, num):
+        print('Rating\tName\tScire')
+        for i in range(num):
+            univ_info = self.univ_list[i]
+            print(f'{univ_info[0]}\t{univ_info[1]}\t{univ_info[2]}')
+
+    def main(url, num):
+        instance = BestChinaUniv()
+        instance.get_html(url)
+        instance.fill_univ_list()
+        instance.print_univ_list(num)
+
+
+if __name__ == '__main__':
+    url = 'http://www.zuihaodaxue.com/zuihaodaxuepaiming2018.html'
+    BestChinaUniv.main(url, 20)
+
